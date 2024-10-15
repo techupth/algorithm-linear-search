14b5c0c297bd88270d764f661183dc2b0f9ab2b6
CodePawsDev <isripen1991@outlook.com>
2024-10-15 19:30:29 +0700

    feat: add answer to exercise in thai language

--- th/ex-1-2.js
+++ th/ex-1-2.js
@@ -11,6 +11,15 @@
 
 // Start coding here
 
+function findIfProductsExist(products, targetProduct) {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].name === targetProduct) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 const products = [
   { id: 2, name: "Desk Chair", price: 300, category: "Furniture", zone: "C" },
   { id: 8, name: "Keyboard", price: 100, category: "Electronics", zone: "E" },
